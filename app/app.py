74b9dba0710e64919eb2cbcefd2116b6d1d391fe
hasegawa_itsuki <ii440092@icloud.com>
2022-01-30 10:48:03 +0900

    first commit

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 # Flaskとrender_template（HTMLを表示させるための関数）をインポート
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 # Flaskオブジェクトの生成
 app = Flask(__name__)
 
@@ -14,7 +13,16 @@ def hello():
 # 「/index」へアクセスがあった場合に、「index.html」を返す
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    name = request.args.get("name")
+    okyo = ["眠い", "眠くない", "最高"]
+    return render_template("index.html", name=name, okyo=okyo)
+
+
+@app.route("/index", methods=["post"])
+def post():
+    name = request.form["name"]
+    okyo = ["眠い", "眠くない", "最高"]
+    return render_template("index.html", name=name, okyo=okyo)
 
 
 # おまじない
