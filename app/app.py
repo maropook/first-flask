da2e805fb8785b24accbd885ec7e0f6fbee95659
hasegawa_itsuki <ii440092@icloud.com>
2022-01-31 11:42:17 +0900

    first commit

--- app/app.py
+++ app/app.py
@@ -1,30 +1,104 @@
-# Flaskとrender_template（HTMLを表示させるための関数）をインポート
-from flask import Flask, render_template, request
-# Flaskオブジェクトの生成
+from flask import Flask,render_template,request,session,redirect,url_for
+from models.models import OnegaiContent,User
+from models.database import db_session
+from datetime import datetime
+from app import key
+
+from hashlib import sha256
+
 app = Flask(__name__)
+app.secret_key = key.SECRET_KEY
 
 
-# 「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 @app.route("/")
-def hello():
-    return "Hello World"
-
-
-# 「/index」へアクセスがあった場合に、「index.html」を返す
 @app.route("/index")
 def index():
-    name = request.args.get("name")
-    okyo = ["眠い", "眠くない", "最高"]
-    return render_template("index.html", name=name, okyo=okyo)
+    if "user_name" in session:
+        name = session["user_name"]
+        all_onegai = OnegaiContent.query.all()
+        return render_template("index.html",name=name,all_onegai=all_onegai)
+    else:
+        return redirect(url_for("top",status="logout"))
 
 
-@app.route("/index", methods=["post"])
-def post():
-    name = request.form["name"]
-    okyo = ["眠い", "眠くない", "最高"]
-    return render_template("index.html", name=name, okyo=okyo)
+@app.route("/add",methods=["post"])
+def add():
+    title = request.form["title"]
+    body = request.form["body"]
+    content = OnegaiContent(title,body,datetime.now())
+    db_session.add(content)
+    db_session.commit()
+    return redirect(url_for("index"))
 
 
-# おまじない
+@app.route("/update",methods=["post"])
+def update():
+    content = OnegaiContent.query.filter_by(id=request.form["update"]).first()
+    content.title = request.form["title"]
+    content.body = request.form["body"]
+    db_session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/delete",methods=["post"])
+def delete():
+    id_list = request.form.getlist("delete")
+    for id in id_list:
+        content = OnegaiContent.query.filter_by(id=id).first()
+        db_session.delete(content)
+    db_session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/top")
+def top():
+    status = request.args.get("status")
+    return render_template("top.html",status=status)
+
+
+@app.route("/login",methods=["post"])
+def login():
+    user_name = request.form["user_name"]
+    user = User.query.filter_by(user_name=user_name).first()
+    if user:
+        password = request.form["password"]
+        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        if user.hashed_password == hashed_password:
+            session["user_name"] = user_name
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("top",status="wrong_password"))
+    else:
+        return redirect(url_for("top",status="user_notfound"))
+
+
+@app.route("/newcomer")
+def newcomer():
+    status = request.args.get("status")
+    return render_template("newcomer.html",status=status)
+
+
+@app.route("/registar",methods=["post"])
+def registar():
+    user_name = request.form["user_name"]
+    user = User.query.filter_by(user_name=user_name).first()
+    if user:
+        return redirect(url_for("newcomer",status="exist_user"))
+    else:
+        password = request.form["password"]
+        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        user = User(user_name, hashed_password)
+        db_session.add(user)
+        db_session.commit()
+        session["user_name"] = user_name
+        return redirect(url_for("index"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user_name", None)
+    return redirect(url_for("top",status="logout"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
